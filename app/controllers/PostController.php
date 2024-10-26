8eb43b459e5272437da5abbc47406d8ed38500aa
agnesvarga1 <agikolyok@gmail.com>
2024-10-26 13:36:59 +0200

    feat redirct user to dashboard after login

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -1,8 +1,16 @@
 <?php
-
+require_once '../app/models/Post.php';
 
 class PostController{
     public function index(){
-        require '../app/views/home.php';
+        session_start();
+
+        if(isset( $_SESSION['user_id'])){
+            require '../app/views/dashboard.php'; // Redirect to user dashboard
+        }else{
+            require '../app/views/home.php';
+        }
+       
+       
     }
 }
