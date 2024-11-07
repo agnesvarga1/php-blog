dc06574bcf96291a89adba0dec13e5003cd967c2
agnesvarga1 <agikolyok@gmail.com>
2024-11-07 12:00:06 +0100

    fix swap img + date col in index table

--- app/views/Dashboard.php
+++ app/views/Dashboard.php
@@ -32,12 +32,13 @@ include '../app/views/partials/navbar.php';  // Include il partial della navbar
       <td><?=$post['title']; ?></td>
       <td class=" text-truncate" style="max-width: 180px;"><?=$post['content']; ?></td>
       <td class="text-primary fw-semibold" ><?=$post['category_id']; ?></td>
-      <td><?=$post['updated_at']; ?></td>
+     
       <?php if(isset($post['image'])):?>
       <td><img style="width:70px;" src="<?= $post['image'] ?>" alt="post-image thumbnail"></td>
       <?php else:?>
         <td>No image</td>
       <?php endif;?>
+      <td><?=$post['updated_at']; ?></td>
       <td><a href="http://localhost:8888/php-blog/public/postupdate?id=<?= $post['id']; ?>" class= "fw-semibold text-primary fs-2 text-decoration-none">Edit</a> 
       <a href="http://localhost:8888/php-blog/public/deletepost?id=<?= $post['id']; ?>"  class= "fw-semibold text-danger fs-2 text-decoration-none">  Delete</a></td>
     </tr>
